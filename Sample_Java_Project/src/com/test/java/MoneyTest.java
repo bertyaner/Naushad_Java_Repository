d0c1556ad1907fe33dd4782c3029b5099d638959
bertyaner <naushad.wb@gmail.com>
2020-04-22 01:32:31 +0530

    new addition of junit file

---
+++ Sample_Java_Project/src/com/test/java/MoneyTest.java
@@ -0,0 +1,20 @@
+package com.test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.main.java.Money;
+
+class MoneyTest {
+
+	@Test
+	void testMoney() {
+		//fail("Not yet implemented");
+		Money money = new Money(10, "USD", "abc");
+		assertEquals(10, money.getAmount(),"working fine for integer");
+		assertEquals("USD", money.getCurrency(),"working fine for string");
+		assertNull(money.getObject(),"it is null");
+	}
+
+}
